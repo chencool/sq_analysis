f93d2a0b1753383dedbf269801b43eb736071c34
phimathx <271749337@qq.com>
2018-12-13 13:34:22 +0800

    add new files

---
+++ 2.service/ShQProject/Models/FTANoteGate.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Dxc.Shq.WebApi.Models
+{
+    public class FTANoteGate
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+
+        [ForeignKey("FTANoteGateType")]
+        public string GateTypeName { get; set; }
+        public FTANoteGateType FTANoteGateType { get; set; }
+
+        [ForeignKey("FTAProject")]
+        public Guid FTAProjectId { get; set; }
+
+        public FTAProject FTAProject { get; set; }
+    }
+}
---
+++ 2.service/ShQProject/Models/FTANoteGateType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Dxc.Shq.WebApi.Models
+{
+    public class FTANoteGateType
+    {
+        [Key]
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+    }
+}
